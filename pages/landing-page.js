c2ae757346c00dce7ac7880101b73a1d2fb37156
Chris <chrisliu0416@gmail.com>
2020-11-30 10:52:43 -0800

    test update

--- pages/landing-page.js
+++ pages/landing-page.js
@@ -50,7 +50,7 @@ export default function LandingPage({ ...rest }) {
         <div className={classes.container}>
           <GridContainer>
             <GridItem xs={12} sm={6} md={6}>
-              <h1 className={classes.title}>Your Story Starts With Us.</h1>
+              <h1 className={classes.title}>Your Story Starts With Me.</h1>
               <h4>
                 Every landing page needs a small description after the big bold
                 title, that{"'"}s why we added this text here. Add here all the
