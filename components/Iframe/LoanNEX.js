59ea98f55ad00c23b5c9fb41242c86e52163900f
Chris <chrisliu0416@gmail.com>
2021-04-16 14:59:26 -0700

    spelling

--- components/Iframe/LoanNEX.js
+++ components/Iframe/LoanNEX.js
@@ -12,7 +12,7 @@ const Iframe = () => {
     return (
         // basic bootstrap classes. you can change with yours.
         <div>
-            <iframe src="https://acralending.com/loanNEX/corr/" frameBorder="0" scrolling="yes" width="100%" height="1400px" ></iframe>
+            <iframe src="https://acralending.com/LoanNEX/corr/" frameBorder="0" scrolling="yes" width="100%" height="1400px" ></iframe>
         </div>
     );
 };
