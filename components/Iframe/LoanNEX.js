103571d2a44e46ffbd68a415b8f0b2fd74ef6222
Chris <chrisliu0416@gmail.com>
2021-05-20 09:16:32 -0700

    quick qualifier back up

--- components/Iframe/LoanNEX.js
+++ components/Iframe/LoanNEX.js
@@ -12,8 +12,7 @@ const Iframe = () => {
     return (
         // basic bootstrap classes. you can change with yours.
         <div>
-            <p style={{ textAlign: "center", color: "black" }}>The Quick Qualifier is down for maintenance. Sorry for the inconvenience.</p>
-            {/* <iframe src="https://acralending.com/LoanNEX/corr/" frameBorder="0" scrolling="yes" width="100%" height="1400px" ></iframe> */}
+            <iframe src="https://acralending.com/LoanNEX/corr/" frameBorder="0" scrolling="yes" width="100%" height="1400px" ></iframe>
         </div>
     );
 };
