07544e7eb695ed851e7f67cd74b27b7ee6ee6246
Chris Liu <chrisl@CSC.local>
2023-06-16 14:49:05 -0700

    Add next images

--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
 const withPlugins = require("next-compose-plugins");
-// const withImages = require("next-images");
+const withImages = require("next-images");
 // const withSass = require("@zeit/next-sass");
 // const withCSS = require("@zeit/next-css");
 const webpack = require("webpack");
 const path = require("path");
 
-// module.exports = withPlugins([[withSass], [withImages], [withCSS]], {
-  module.exports = {
+module.exports = withPlugins([[withImages]], {
+  // module.exports = withPlugins([[withSass], [withImages], [withCSS]], {
+  // module.exports = {
   webpack(config, options) {
     config.resolve.modules.push(path.resolve("./"));
     config.module.rules.push({
@@ -20,5 +21,5 @@ const path = require("path");
     });
     return config;
   }
-};
-// });
+// };
+});
